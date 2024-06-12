24136ec57832242effe7b3add9883889926e8b22
Senthur <imsenthur@gmail.com>
2024-06-12 14:34:34 -0400

    Fixed file paths.

--- tests/real_time_test.sh
+++ tests/real_time_test.sh
@@ -18,14 +18,14 @@ if [ -z "$dur" ]; then
     dur=1
 fi
 echo "Running cyclictest for $dur minute"
-cyclictest -D"$dur"m -m -Sp90 -i200 -h400 -q >./raw/output
+cyclictest -D"$dur"m -m -Sp90 -i200 -h400 -q > data/raw/output
 
 # 2. Get maximum latency
 max=`grep "Max Latencies" output | tr " " "\n" | sort -n | tail -1 | sed s/^0*//`
 min=`grep "Min Latencies" output | tr " " "\n" | sort -n | head -1 | sed s/^0*//`
 
 # 3. Grep data lines, remove empty lines and create a common field separator
-grep -v -e "^#" -e "^$" output | tr " " "\t" >./histograms/histogram 
+grep -v -e "^#" -e "^$" output | tr " " "\t" >data/histograms/histogram 
 
 # 4. Set the number of cores
 cores=$(nproc)
@@ -34,21 +34,21 @@ cores=$(nproc)
 for i in `seq 1 $cores`
 do
     column=`expr $i + 1`
-    cut -f1,$column ./histograms/histogram >./histograms/histogram$i
+    cut -f1,$column data/histograms/histogram >data/histograms/histogram$i
 
     # create a new file called statistics that has a third row in addition to the histogram1 file that
     # is just the multiplication of the first and second row of the histogram1 file
-    awk '{print $1, $2, $1*$2}' ./histograms/histogram$i > ./analysis/data$i
-    mean=$(awk '{xf+=$3; n+=$2} END {print xf/n}' ./analysis/data$i)
-    echo "Mean: $mean" > ./analysis/statistics$i
+    awk '{print $1, $2, $1*$2}' data/histograms/histogram$i > data/analysis/data$i
+    mean=$(awk '{xf+=$3; n+=$2} END {print xf/n}' data/analysis/data$i)
+    echo "Mean: $mean" > data/analysis/statistics$i
 
-    awk -v mean="$mean" '{print $1, $2, $3, $2*($1-mean)^2, $2*($1-mean)^2}' ./analysis/data$i > ./analysis/data$i.tmp
-    mv ./analysis/data$i.tmp ./analysis/data$i
-    variance=$(awk '{x+=$4; n+=$2} END {print x/(n-1)}' ./analysis/data$i)
-    echo "Variance: $variance" >> ./analysis/statistics$i
+    awk -v mean="$mean" '{print $1, $2, $3, $2*($1-mean)^2, $2*($1-mean)^2}' data/analysis/data$i > data/analysis/data$i.tmp
+    mv data/analysis/data$i.tmp data/analysis/data$i
+    variance=$(awk '{x+=$4; n+=$2} END {print x/(n-1)}' data/analysis/data$i)
+    echo "Variance: $variance" >> data/analysis/statistics$i
 
     std_dev=$(awk -v variance="$variance" 'BEGIN {print sqrt(variance)}')
-    echo "Standard Deviation: $std_dev" >> ./analysis/statistics$i
+    echo "Standard Deviation: $std_dev" >> data/analysis/statistics$i
 
 done
 
