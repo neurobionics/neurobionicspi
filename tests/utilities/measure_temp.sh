b8a85f9952021b932852981add4f6b7204f94b39
Senthur <imsenthur@gmail.com>
2024-06-13 15:09:20 -0400

    Fixed command not found error.

--- tests/utilities/measure_temp.sh
+++ tests/utilities/measure_temp.sh
@@ -3,9 +3,9 @@
 dur=${1:-1}
 frequency=${2:-500}
 
-time_start = $(date +%s)
+timestart=$(date +%s)
 
-while [ $(($(date +%s) - time_start)) -lt $dur*60 ]
+while [ $(($(date +%s) - timestart)) -lt $dur*60 ]
 do
     temp=$(vcgencmd measure_temp)
     temp=${temp:5:2}
