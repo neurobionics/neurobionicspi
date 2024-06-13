8e450923b574e2cf1596d822bbcee3338255e834
Senthur <imsenthur@gmail.com>
2024-06-13 15:49:11 -0400

    Added stressors.

---
+++ tests/utilities/add_stress.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dur=${1:-1}
+load=${2:-50}
+
+check_command() {
+    if ! command -v "$1" &> /dev/null; then
+        echo "$1 could not be found"
+        echo "Please install $1"
+        exit 1
+    fi
+}
+
+check_command stress-ng
+
+stress-ng -c 1 --mq 1 --iomix 1 --vm 1 --vm-bytes 50% -l $load --cpu-method fft --metrics-brief --tz -t $dur
