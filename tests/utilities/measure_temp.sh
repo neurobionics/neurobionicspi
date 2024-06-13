62a7135b0f33bf6d223cd18d94258c522629d5df
Senthur <imsenthur@gmail.com>
2024-06-13 15:10:29 -0400

    Fixed integer expression error in dur*60

--- tests/utilities/measure_temp.sh
+++ tests/utilities/measure_temp.sh
@@ -4,8 +4,9 @@ dur=${1:-1}
 frequency=${2:-500}
 
 timestart=$(date +%s)
+dur_in_seconds=$((dur * 60))
 
-while [ $(($(date +%s) - timestart)) -lt $dur*60 ]
+while [ $(($(date +%s) - timestart)) -lt $dur_in_seconds ]
 do
     temp=$(vcgencmd measure_temp)
     temp=${temp:5:2}
