2ecd945ded159c683d21ce537e939dc81156986f
Senthur <imsenthur@gmail.com>
2024-06-13 10:43:50 -0400

    Added offline mode and analysis data (us)

--- tests/real_time_test.sh
+++ tests/real_time_test.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-# Check if cyclictest, stress-ng, and gnuplot are installed
-check_command() {
-    if ! command -v "$1" &> /dev/null; then
-        echo "$1 could not be found"
-        echo "Please install $1"
-        exit 1
+offline=1
+enable_plotting=0
+
+if [ "$offline" -eq 1 ]; then
+    echo "Running offline analysis"
+    cores=4
+else
+    echo "Running tests online"
+    # Check if cyclictest, stress-ng, and gnuplot are installed
+    check_command() {
+        if ! command -v "$1" &> /dev/null; then
+            echo "$1 could not be found"
+            echo "Please install $1"
+            exit 1
+        fi
+    }
+
+    check_command cyclictest
+    check_command stress-ng
+    check_command gnuplot
+
+    dur=$1
+    if [ -z "$dur" ]; then
+        dur=1
     fi
-}
-
-check_command cyclictest
-check_command stress-ng
-check_command gnuplot
-
-dur=$1
-if [ -z "$dur" ]; then
-    dur=1
+    echo "Running cyclictest for $dur minute"
+    cyclictest --nsecs -D"$dur"m -m -Sp90 -i200 -h350000 -q > $PWD/data/raw/output
+    cores=$(nproc)
 fi
-echo "Running cyclictest for $dur minute"
-cyclictest --nsecs -D"$dur"m -m -Sp90 -i200 -h350000 -q > $PWD/data/raw/output
 
 # 2. Get maximum latency
 # max=`grep "Max Latencies" data/raw/output | tr " " "\n" | sort -n | tail -1 | sed s/^0*//`
@@ -28,7 +38,6 @@ cyclictest --nsecs -D"$dur"m -m -Sp90 -i200 -h350000 -q > $PWD/data/raw/output
 grep -v -e "^#" -e "^$" $PWD/data/raw/output | tr " " "\t" > $PWD/data/histograms/histogram 
 
 # 4. Set the number of cores
-cores=$(nproc)
 read -r -a max <<< $(grep "Max Latencies" $PWD/data/raw/output | awk -F: '{print $2}')
 read -r -a min <<< $(grep "Min Latencies" $PWD/data/raw/output | awk -F: '{print $2}')
 
@@ -54,37 +63,38 @@ do
 
     std_dev=$(awk -v variance="$variance" 'BEGIN {print sqrt(variance)}')
     echo "Standard Deviation: $std_dev" >> $PWD/data/analysis/statistics$i
-
 done
 
-# 6. Create plot command header
-echo -n -e "set title \"Worst Case Latency Test \"\n\
-set terminal png\n\
-set xlabel \"Latency (ns) [Max Latencies:${max[@]} ns]\"\n
-set logscale y\n\
-set xrange [0:350000]\n\
-set yrange [0.8:*]\n\
-set ylabel \"Number of latency samples\"\n\
-set output \"$PWD/data/plots/plot.png\"\n\
-plot " >$PWD/data/plots/plotcmd
+if [ "$enable_plotting" -eq 1 ]; then
+    # 6. Create plot command header
+    echo -n -e "set title \"Worst Case Latency Test \"\n\
+    set terminal png\n\
+    set xlabel \"Latency (ns) [Max Latencies:${max[@]} ns]\"\n
+    set logscale y\n\
+    set xrange [0:350000]\n\
+    set yrange [0.8:*]\n\
+    set ylabel \"Number of latency samples\"\n\
+    set output \"$PWD/data/plots/plot.png\"\n\
+    plot " >$PWD/data/plots/plotcmd
 
-# 7. Append plot command data references
-for i in `seq 1 $cores`
-do
-  if test $i != 1
-  then
-    echo -n ", " >> $PWD/data/plots/plotcmd
-  fi
-  cpuno=`expr $i - 1`
-  if test $cpuno -lt 10
-  then
-    title=" CPU$cpuno"
-   else
-    title="CPU$cpuno"
-  fi
-  echo -n "\"$PWD/data/histograms/histogram$i\" using 1:2 title \"$title\" with histeps" >> $PWD/data/plots/plotcmd
-done
+    # 7. Append plot command data references
+    for i in `seq 1 $cores`
+    do
+    if test $i != 1
+    then
+        echo -n ", " >> $PWD/data/plots/plotcmd
+    fi
+    cpuno=`expr $i - 1`
+    if test $cpuno -lt 10
+    then
+        title=" CPU$cpuno"
+    else
+        title="CPU$cpuno"
+    fi
+    echo -n "\"$PWD/data/histograms/histogram$i\" using 1:2 title \"$title\" with histeps" >> $PWD/data/plots/plotcmd
+    done
 
-# 8. Execute plot command
-gnuplot -persist < $PWD/data/plots/plotcmd
-echo "Histogram plot saved in $PWD/data/plots/plot.png"
+    # 8. Execute plot command
+    gnuplot -persist < $PWD/data/plots/plotcmd
+    echo "Histogram plot saved in $PWD/data/plots/plot.png"
+fi
