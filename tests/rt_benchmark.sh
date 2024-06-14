c84c4113c500a75d0c1ff6b2ff6942c2f1eca51d
Senthur <imsenthur@gmail.com>
2024-06-14 13:31:17 -0400

    Added benchmark script.

---
+++ tests/rt_benchmark.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+load=(100 25 75 50)
+idle_temp=63
+duration=7
+
+while [ ${#load[@]} -gt 0 ]; do
+    temp=$(vcgencmd measure_temp)
+    temp=${temp:5:4}
+
+    # Compare the temperature as an integer
+    if [ "$temp" -lt "$idle_temp" ]; then
+        sudo bash "$PWD/rt_test.sh" --duration $duration --load ${load[0]}
+        
+        load=("${load[@]:1}")
+    else
+        sleep 60
+        continue
+    fi
+done
